f1ed4ca49f0a0a2f66e8774e152c791ce5bcdb52
Tareq Hasan <79653608+tareq2007@users.noreply.github.com>
2025-04-22 20:08:12 +0600

    debug false

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^vn$n^6t&&7(vgk-up$dzld*o!m*j)ju=me^apmmuw=#%h6=qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['devsearch-project-mjjb.onrender.com']
 
